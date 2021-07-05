50ac4de7908af7547ebada5121385d795ec3736f
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-07-05 14:28:16 +0100

    Update round robin.py

--- round robin.py
+++ round robin.py
@@ -1,3 +1,14 @@
 import random
 
 print("Welcome to my round robin app")
+
+teams=int(input("How many teams are there?: "))
+team_list=[]
+team_count=0
+
+for i in range (teams):
+    team_count+=1
+    entry=input("Type in name of team {}: ".format(i+1))
+    team_list.append(entry)
+    
+print (team_list)
